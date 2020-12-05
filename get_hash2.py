7cd063940c51804c65e8fdebe58c43d999423622
Mihails Prihodko <mihails.prihodko@gmail.com>
2020-12-05 16:01:25 +0200

    hashing script was improved after some use

--- get_hash2.py
+++ get_hash2.py
@@ -1,35 +1,37 @@
 """
 This scropt gets file hashes
 
-How to use this sctipt:
-TODO
+How to use this sctipt
+
+To hash the whole arvhive:
+python get_hash2.py path_to_archive
+
+To hash the one specific volume:
+python get_hash2.py path_to_archive volume volume_dir_name
 
 The script was tested with Python 3.9
-At least Python 3.9 is required
 """
 import os
+import sys
 import hashlib
 import json
 import datetime
 import posixpath
 import logging
-import configparser
 
-start_datetime = datetime.datetime.now()
-start_datetime_txt = start_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-
-config = configparser.ConfigParser()
-config.read("get_hash.ini")
-
-READ_BLOCK_SIZE = 4096
+####################################### Defines ####################################################
 MD5 = "MD5"
 SHA256 = "SHA256"
 SHA512 = "SHA512"
 
-VOLUME_ROOT = os.path.realpath(config["GET_HASH"]["volume_root"])
-HASH_DIR_ROOT = os.path.join(VOLUME_ROOT, "hashes")
+####################################### Config #####################################################
+
 HASH_ALGORITHM = SHA256
-DIR_TO_HASH = config["GET_HASH"]["dir_to_hash"]
+READ_BLOCK_SIZE = 4096
+LOG_LEVEL = logging.INFO
+LOG_ENCODING = "utf-8"
+
+####################################### Globals ####################################################
 
 ####################################################################################################
 
@@ -47,9 +49,9 @@ def get_file_hash(fname, hash_type):
         for chunk in iter(lambda: f.read(READ_BLOCK_SIZE), b""):
             hash.update(chunk)
     return hash.hexdigest()
-    
+
 def save_hashes_for_files(root_dir_abspath, file_abspath_lst, hashes_file_abspath):
-    index = {"algorithm": HASH_ALGORITHM, "datetime": start_datetime.strftime("%Y:%m:%d %H:%M:%S")}
+    logging.info("Saving hashes files in archive root")
     hashes_lst = []
     for src_abspath in file_abspath_lst:
         src_relpath = os.path.relpath(src_abspath, root_dir_abspath) # path relative to root, which is easy to compare between different drives"
@@ -57,71 +59,90 @@ def save_hashes_for_files(root_dir_abspath, file_abspath_lst, hashes_file_abspat
         logging.debug("Processing file [%s]" % (src_abspath))
         print("Processing file [%s]" % (src_abspath))
         hashes_lst.append({"posixpath": posixpath.normpath(src_relpath), "hash": hash})
-    index["file_data"] = hashes_lst
 
-    # j = json.dumps(hashes_lst)
     logging.info("Saving hashes to file... [%s]" % hashes_file_abspath)
     with open(hashes_file_abspath, "w") as f:
-        json.dump(index, f, indent=4)
+        json.dump(hashes_lst, f, indent=4)
     logging.info("Done [%s]" % hashes_file_abspath)
 
-def save_hashes_for_dir(root_dir_abspath, hashes_file_abspath):
-    logging.info("Saving hashes for directory at path [%s]..." % root_dir_abspath)
-    index = {"algorithm": HASH_ALGORITHM, "datetime": start_datetime.strftime("%Y:%m:%d %H:%M:%S")}
+def save_hashes_for_volume(achive_root_abspath, volume_dir_abspath, hashes_file_abspath):
+    logging.info("Saving hashes for directory at path [%s]" % volume_dir_abspath)
     hashes_lst = []
-    for path, directories, files in os.walk(root_dir_abspath):
+    for path, directories, files in os.walk(volume_dir_abspath):
         for file in files:
             filename, file_extension = os.path.splitext(file)
             if file_extension.upper() == ".JSON":
                 continue
             src_abspath = os.path.join(os.path.abspath(path), file)
-            src_relpath = os.path.relpath(src_abspath, root_dir_abspath) # path relative to root, which is easy to compare between different drives"
+            src_relpath = os.path.relpath(src_abspath, achive_root_abspath) # path relative to root, which is easy to compare between different drives"
             hash = get_file_hash(src_abspath, HASH_ALGORITHM)
             logging.debug("Processing file [%s]" % (src_abspath))
             print("Processing file [%s]" % (src_abspath))
             hashes_lst.append({"posixpath": posixpath.normpath(src_relpath), "hash": hash})
-    index["file_data"] = hashes_lst
 
-    # j = json.dumps(hashes_lst)
     logging.info("Saving hashes to file... [%s]" % hashes_file_abspath)
     with open(hashes_file_abspath, "w") as f:
-        json.dump(index, f, indent=4)
+        json.dump(hashes_lst, f, indent=4)
     logging.info("Done [%s]" % hashes_file_abspath)
 
-if __name__ == "__main__":
-    if not os.path.exists(HASH_DIR_ROOT):
-        os.makedirs(HASH_DIR_ROOT)
 
-    logfile = os.path.join(HASH_DIR_ROOT, "log_%s.txt" % start_datetime_txt)
+####################################################################################################
+
+if __name__ == "__main__":
+    start_datetime = datetime.datetime.now()
+    start_datetime_txt = start_datetime.strftime("%Y-%m-%d__%H-%M-%S")
+
+    if len(sys.argv) < 2:
+        print ("Not enough arguments")
+        exit(0)
+    archieve_root = os.path.abspath(sys.argv[1])
+    if not os.path.isdir(archieve_root):
+        print("There is no directory: [%s]" % archieve_root)
+        exit(0)
+    if len(sys.argv) < 3:
+        volume_to_hash = None  # hash all volumes
+    else:
+        volume_to_hash = sys.argv[2]
+        if not os.path.isdir(os.path.join(archieve_root, volume_to_hash)):
+            print("There is no directory: [%s] in archieve_root [%s]" % (volume_to_hash, archieve_root))
+        
+
+    hash_dir_root = os.path.join(os.path.abspath(archieve_root), "hashes__%s" % start_datetime_txt)
+
+    if not os.path.exists(hash_dir_root):
+        os.makedirs(hash_dir_root)
+
+    logfile = os.path.join(hash_dir_root, "log.txt")
     logging.basicConfig(
         filename=logfile,
-        encoding="utf-8",
-        level=logging.INFO,
+        encoding=LOG_ENCODING,
+        level=LOG_LEVEL,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
-
-    if DIR_TO_HASH == "all":
-        logging.info("Saving hashes for volume at path [%s]..." % VOLUME_ROOT)
-        logging.debug("VOLUME_ROOT = [%s]" % VOLUME_ROOT)
-        logging.debug("HASH_DIR_ROOT = [%s]" % HASH_DIR_ROOT)
-        logging.debug("HASH_ALGORITHM = [%s]" % HASH_ALGORITHM)
-        logging.debug("READ_BLOCK_SIZE = [%s]" % READ_BLOCK_SIZE)
-        
-        files_and_dirs = [os.path.join(VOLUME_ROOT, item) for item in os.listdir(VOLUME_ROOT) if "hashes" not in item]
+    logging.info("Hashing started at: [%s]" % start_datetime_txt)
+    logging.info("archieve_root = [%s]" % archieve_root)
+    logging.info("hash_dir_root = [%s]" % hash_dir_root)
+    logging.info("HASH_ALGORITHM = [%s]" % HASH_ALGORITHM)
+    logging.info("READ_BLOCK_SIZE = [%s]" % READ_BLOCK_SIZE)
+    if volume_to_hash is None:
+        logging.debug("Mode: all volumes in the archive")
+        files_and_dirs = [os.path.join(archieve_root, item) for item in os.listdir(archieve_root) if "hashes" not in item]
         directories = [item for item in files_and_dirs if os.path.isdir(item)]
         files = [item for item in files_and_dirs if os.path.isfile(item)]
 
         for dir_abspath in directories:
-            dir = os.path.relpath(dir_abspath, VOLUME_ROOT)
-            hashes_file = os.path.join(HASH_DIR_ROOT, dir + "_hashes_" + start_datetime_txt + ".json")
-            save_hashes_for_dir(dir_abspath, hashes_file)
+            dir = os.path.relpath(dir_abspath, archieve_root)
+            hashes_file = os.path.join(hash_dir_root, dir + "_hashes.json")
+            save_hashes_for_volume(archieve_root, dir_abspath, hashes_file)
             
         if len(files) > 0:
-            hashes_file = os.path.join(HASH_DIR_ROOT, "files_hashes_" + start_datetime_txt + ".json")
-            save_hashes_for_files(VOLUME_ROOT, files, hashes_file)
-    else:
-        dir_abspath = os.path.join(VOLUME_ROOT, DIR_TO_HASH)
-        dir = DIR_TO_HASH
-        hashes_file = os.path.join(HASH_DIR_ROOT, dir + "_hashes_" + start_datetime_txt + ".json")
-        save_hashes_for_dir(dir_abspath, hashes_file)
+            hashes_file = os.path.join(hash_dir_root, "files_hashes.json")
+            save_hashes_for_files(archieve_root, files, hashes_file)
+    else: # specific volume
+        logging.debug("Mode: specific volume: [%s]" % volume_to_hash)
+        dir_abspath = os.path.join(archieve_root, volume_to_hash)
+        dir = volume_to_hash
+        hashes_file = os.path.join(hash_dir_root, dir + "_hashes.json")
+        save_hashes_for_volume(archieve_root, dir_abspath, hashes_file)
 
+    logging.info("Hashing ended at: [%s]" % datetime.datetime.now().strftime("%Y-%m-%d__%H-%M-%S"))
